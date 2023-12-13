f932b0d70990310e006c23a16fe3eb825a97d21f
cemilecaglar <cemilecaglartahvil@gmail.com>
2023-12-13 16:19:11 +0300

    // git denemesi 16:16

--- yazilideneme/Form1.cs
+++ yazilideneme/Form1.cs
@@ -16,5 +16,10 @@ namespace yazilideneme
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // git denemesi 16:16
+        }
     }
 }
